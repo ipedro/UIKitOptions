2b467cd4acfd8128c89685f5a7ee9d6f3b519737
Pedro Almeida <ip4dro@gmail.com>
2021-04-16 18:19:40 +0200

    Improve LayoutCompressionOptions defaults

--- Sources/UIKitOptions/UIView+Options/UIView+LayoutCompressionOptions.swift
+++ Sources/UIKitOptions/UIView+Options/UIView+LayoutCompressionOptions.swift
@@ -11,32 +11,39 @@ public extension UIView {
     /// Applies the layout compression options to the view instance.
     /// - Parameter options: The view layout compresison options.
     func applyOptions(_ options: UIView.LayoutCompressionOptions) {
-        setContentCompressionResistancePriority(options.horizontalCompressionResistance, for: .horizontal)
-        setContentCompressionResistancePriority(options.verticalCompressionResistance, for: .vertical)
-        
-        setContentHuggingPriority(options.horizontalHuggingPriority, for: .horizontal)
-        setContentHuggingPriority(options.verticalHuggingPriority, for: .vertical)
+        if let horizontalCompressionResistance = options.horizontalCompressionResistance {
+            setContentCompressionResistancePriority(horizontalCompressionResistance, for: .horizontal)
+        }
+        if let verticalCompressionResistance = options.verticalCompressionResistance {
+            setContentCompressionResistancePriority(verticalCompressionResistance, for: .vertical)
+        }
+        if let horizontalHuggingPriority = options.horizontalHuggingPriority {
+            setContentHuggingPriority(horizontalHuggingPriority, for: .horizontal)
+        }
+        if let verticalHuggingPriority = options.verticalHuggingPriority {
+            setContentHuggingPriority(verticalHuggingPriority, for: .vertical)
+        }
     }
     
     /// Describes the view's layout compression and hugging priorities.
     struct LayoutCompressionOptions: Equatable {
         /// The priority with which a view resists being made smaller than its intrinsic width.
-        public var horizontalCompressionResistance: UILayoutPriority
+        public var horizontalCompressionResistance: UILayoutPriority?
         
         /// The priority with which a view resists being made larger than its intrinsic width.
-        public var horizontalHuggingPriority: UILayoutPriority
+        public var horizontalHuggingPriority: UILayoutPriority?
         
         /// The priority with which a view resists being made smaller than its intrinsic height.
-        public var verticalCompressionResistance: UILayoutPriority
+        public var verticalCompressionResistance: UILayoutPriority?
         
         /// The priority with which a view resists being made larger than its intrinsic height.
-        public var verticalHuggingPriority: UILayoutPriority
+        public var verticalHuggingPriority: UILayoutPriority?
         
         public init(
-            horizontalCompressionResistance: UILayoutPriority = .defaultLow,
-            horizontalHugging: UILayoutPriority = .defaultLow,
-            verticalCompressionResistance: UILayoutPriority = .defaultLow,
-            verticalHugging: UILayoutPriority = .defaultLow
+            horizontalCompressionResistance: UILayoutPriority? = nil,
+            horizontalHugging: UILayoutPriority? = nil,
+            verticalCompressionResistance: UILayoutPriority? = nil,
+            verticalHugging: UILayoutPriority? = nil
         ) {
             self.horizontalCompressionResistance = horizontalCompressionResistance
             self.horizontalHuggingPriority       = horizontalHugging
